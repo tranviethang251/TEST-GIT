acd1dad8bbadf6e75e2a3c8a57bef94355ce79e7
Thang <vietthang2512004@gmail.com>
2023-04-17 19:03:29 +0700

    tai len lan1

---
+++ 121 cpp.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cmath> 
+using namespace std ;
+double round(double x , int n) ;
+double round(double x , int n) 
+{
+   double result ;
+   double scale = pow(10 , n) ;
+   x *= scale ;
+   if ( x>= 0)
+   {
+   	result = floor(x+0.5)/scale ;
+	 
+   } else 
+   {
+   	result = - floor(-x + 0.5) / scale ;
+   }
+   return result ;
+   
+}
+int main()
+{
+	double a, y ;
+	int m ;
+	cout << "Input a = " ;
+	cin >> a ;
+	cout << "Round to m = " ;
+	cin >> m ;
+	y = round(a, m );
+	cout << " Ket qua la " << y << endl;
+	return 1 ;
+}
